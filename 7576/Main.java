a726efb79b270eec18f5e62fb15a66296c61bb4d
zesow <anstkd07@gmail.com>
2018-07-11 22:42:52 +0900

    20180711

---
+++ 7576/Main.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Main{
+
+	private static int m,n,max = 0;
+	private static int[][] a,d;
+	
+	private static class Point{
+		int i,j;
+		Point(int i,int j){
+			this.i = i;
+			this.j = j;
+		}
+	}
+	private static Queue<Point> q = new LinkedList<>();
+	
+	public static boolean isNotOut(int i,int j){
+		if(i >= 0 && i < n && j >=0 && j < m)
+			return true;
+		return false;
+	}
+	private static int[] ai = {1,0,-1,0};
+	private static int[] aj = {0,1,0,-1};
+	public static void bfs(){
+		
+		while(!q.isEmpty()){
+			Point temp = q.poll();
+			int i = temp.i;
+			int j = temp.j;
+			if(a[i][j] > max)
+				max = a[i][j];
+
+			for(int k = 0 ; k < 4; ++k){
+				int ni = i +ai[k];
+				int nj = j + aj[k];
+
+				if(isNotOut(ni,nj) == true && a[ni][nj] == 0){
+					a[ni][nj] = a[i][j] + 1;
+					q.add(new Point(ni,nj));
+				}
+			}
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] line = in.readLine().split(" ");
+		m = Integer.parseInt(line[0]);
+		n = Integer.parseInt(line[1]);
+
+		a = new int[n][m];
+		d = new int[n][m];
+
+		for(int i = 0 ; i < n; ++i){	
+			line = in.readLine().split(" ");
+			for(int j = 0 ; j < m ; ++j){
+				a[i][j] = Integer.parseInt(line[j]);
+			}
+		}
+
+		for(int i = 0 ; i < n ; ++i){
+			for(int j = 0 ; j < m ; ++j){
+				if(a[i][j] == 1){
+					q.add(new Point(i,j));
+				}
+			}
+		}
+
+		bfs();
+
+		int flag = 0;
+		for(int i = 0 ; i < n ; ++i){
+			for(int j = 0 ; j < m ; ++j){
+				if(a[i][j] == 0){
+					flag = 1;
+					break;
+				}
+			}
+		}
+
+		if(flag == 1)
+			System.out.println(-1);
+		else
+			System.out.println(max-1);
+	}
+}
